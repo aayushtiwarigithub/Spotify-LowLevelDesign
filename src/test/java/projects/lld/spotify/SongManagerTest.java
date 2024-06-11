4a4b0d5f081090021c477516d6d8ff68f11f6e41
aayushtiwarigithub <aayushattiwari@gmail.com>
2024-06-11 23:53:01 +0530

    added Test classes

---
+++ src/test/java/projects/lld/spotify/SongManagerTest.java
@@ -0,0 +1,4 @@
+package projects.lld.spotify;
+
+public class SongManagerTest {
+}
---
+++ src/test/java/projects/lld/spotify/UserManagerTest.java
@@ -0,0 +1,11 @@
+package projects.lld.spotify;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class UserManagerTest {
+
+    @BeforeEach
+    void setup() {
+
+    }
+}
